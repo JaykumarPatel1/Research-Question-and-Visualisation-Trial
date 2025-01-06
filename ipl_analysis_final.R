2c5185d4dca5091fe71e54fd92dbe41828b31ff0
JaykumarPatel1 <jp23adl@herts.ac.uk>
2025-01-06 10:03:37 +0530

    Add files via upload
    More data filtered and final coding done

---
+++ ipl_analysis_final.R
@@ -0,0 +1,70 @@
+# Load necessary libraries
+library(ggplot2)
+library(dplyr)
+
+# Load the data
+matches <- read.csv("matches.csv")
+str(matches)
+
+# converting variable types
+matches$city = as.factor(matches$city)
+matches$date = as.Date(matches$date)
+matches$team1 = as.factor(matches$team1)
+matches$team2 = as.factor(matches$team2)
+matches$toss_winner = as.factor(matches$toss_winner)
+matches$toss_decision = as.factor(matches$toss_decision)
+matches$result = as.factor(matches$result)
+matches$player_of_match = as.factor(matches$player_of_match)
+matches$venue = as.factor(matches$venue)
+matches$umpire1 = as.factor(matches$umpire1)
+matches$umpire2 = as.factor(matches$umpire2)
+matches$umpire3 = as.factor(matches$umpire3)
+
+# summary of attributes
+summary(matches)
+
+# removing umpire3 (3rd umpire) as all values are missing
+matchesprep = subset(matches, select = -c(umpire3))
+
+# variable to indicate if toss winner is the match winner
+matchesprep$toss_and_match_win <- ifelse(matchesprep$toss_winner == matches$winner, "Yes", "No")
+
+# output pre-processed dataset
+write.csv(x = matchesprep, file = 'matches_clean.csv',row.names = FALSE)
+
+# Hypothesis Testing
+# Chi-square test for independence
+table_toss <- table(matchesprep$toss_and_match_win)
+chi_test <- chisq.test(table_toss)
+
+print(chi_test)
+
+
+# Plot: Proportion of matches won by toss winners
+ggplot(matchesprep, aes(x = toss_and_match_win, fill = toss_and_match_win)) +
+  geom_bar(stat = "count") +
+  ggtitle("Proportion of Matches Won by Toss Winners") +
+  xlab("Toss Winner Also Won Match") +
+  ylab("Count") +
+  theme_minimal()
+
+
+# Additional Plot: Win margin(runs+wickets) based on toss winner or not
+matchesprep$win_margin <- matchesprep$win_by_runs + matchesprep$win_by_wickets
+
+# Hypothesis Testing for Win Margin
+# Subset data for the two groups
+win_margin_yes <- matchesprep$win_margin[matchesprep$toss_and_match_win == "Yes"]
+win_margin_no <- matchesprep$win_margin[matchesprep$toss_and_match_win == "No"]
+
+# two-sample t-test
+t_test <- t.test(win_margin_yes, win_margin_no, alternative = "two.sided", var.equal = FALSE)
+
+print(t_test)
+
+ggplot(matchesprep, aes(x = toss_and_match_win, y = win_margin, fill = toss_and_match_win)) +
+  geom_boxplot() +
+  ggtitle("Win Margin Based on Toss Win") +
+  xlab("Toss Winner Also Won Match") +
+  ylab("Win Margin (Runs + Wickets)") +
+  theme_minimal()
